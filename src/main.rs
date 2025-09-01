8daf47a9bac47f0af9e77b81bc6a78e8740cac84
cyb3rkun <cyb0rg@duck.com>
2025-09-01 19:58:10 +0200

    implemented deletion confirmation popu

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,29 @@
 #![allow(dead_code, unused_variables, unused_must_use)]
 use color_eyre::eyre::Result;
+use ratatui::layout::Alignment;
 use ratatui::{
 	DefaultTerminal, Frame,
 	crossterm::{
 		event::{self, Event},
 		style::Color,
 	},
-	layout::{Constraint, Layout},
-	style::Stylize,
-	widgets::{Block, BorderType, List, ListItem, Paragraph, Widget},
+	layout::{Constraint, Direction, Layout, Rect},
+	style::{Style, Stylize},
+	widgets::{
+		Block, BorderType, Borders, List, ListItem, ListState, Paragraph,
+		Widget, Wrap,
+	},
 };
-#[derive(Debug, Default)]
+#[derive(Debug)]
 struct AppState {
 	plans: Vec<Plan>,
+	list_state: ListState,
+	popup: Popup,
+}
+#[derive(Debug)]
+enum Popup {
+	None,
+	ConfirmDelete(usize),
 }
 
 #[derive(Debug, Default)]
@@ -23,7 +34,11 @@ struct Plan {
 }
 
 fn main() -> Result<()> {
-	let mut state = AppState::default();
+	let mut state = AppState {
+		plans: vec![],
+		list_state: ListState::default(),
+		popup: Popup::None,
+	};
 	for i in 0..10 {
 		state.plans.push(Plan {
 			title: "Test Item 1".to_string(),
@@ -42,26 +57,71 @@ fn main() -> Result<()> {
 }
 fn run(mut terminal: DefaultTerminal, app_state: &mut AppState) -> Result<()> {
 	loop {
+		//rendering
 		terminal.draw(|f| render(f, app_state))?;
 		//input_handling
 		// blocking thread for some reason!
-		if let Event::Key(key) = event::read()? {
-			match key.code {
-				event::KeyCode::Char('q') => break,
-				_ => {}
+		match app_state.popup {
+			Popup::ConfirmDelete(index) => {
+				if let Event::Key(key) = event::read()? {
+					match key.code {
+						event::KeyCode::Char('y') => {
+							app_state.plans.remove(index);
+							app_state.popup = Popup::None;
+						}
+						event::KeyCode::Char('n')
+						| event::KeyCode::Esc => app_state.popup = Popup::None,
+						_ => {}
+					}
+				}
 			}
-		};
-		//rendering
+			Popup::None => {
+				if let Event::Key(key) = event::read()? {
+					match key.code {
+						event::KeyCode::Char('q') => break,
+						event::KeyCode::Char(c) => {
+							match c {
+								'j' => app_state
+									.list_state
+									.select_next(),
+
+								'k' => app_state
+									.list_state
+									.select_previous(),
+
+								'D' => {
+									if let Some(selected) =
+										app_state
+											.list_state
+											.selected()
+									{
+										verify_deletion(
+											selected,
+											app_state,
+										);
+									}
+								}
+								// '' => {},
+								// '' => {},
+								// '' => {},
+								// '' => {},
+								// '' => {},
+								// '' => {},
+								// '' => {},
+								_ => {}
+							}
+						}
+						_ => {}
+					}
+				};
+			}
+		}
 	}
 	Ok(())
 }
 
-
-fn foo() {
-	bar();
-}
-fn bar() {
-	()
+fn verify_deletion(index: usize, app_state: &mut AppState) {
+	app_state.popup = Popup::ConfirmDelete(index)
 }
 
 fn render(frame: &mut Frame, app_state: &mut AppState) {
@@ -76,11 +136,60 @@ fn render(frame: &mut Frame, app_state: &mut AppState) {
 	let [list_area] = Layout::vertical([Constraint::Fill(1)])
 		.margin(1)
 		.areas(border_area);
-	List::new(
+	let list = List::new(
 		app_state
 			.plans
 			.iter()
 			.map(|i| ListItem::from(i.description.clone())),
 	)
-	.render(list_area, frame.buffer_mut());
+	.highlight_symbol(">")
+	.highlight_style(Style::default().bg(Color::DarkGrey.into()));
+	frame.render_stateful_widget(list, list_area, &mut app_state.list_state);
+
+	if let Popup::ConfirmDelete(index) = app_state.popup {
+		let popup_area = centered_rect(40, 8, frame.area());
+		let block = Block::default()
+			.title("Confirm Deletion")
+			.borders(Borders::ALL)
+			.border_type(BorderType::Thick)
+			.style(Style::default().fg(Color::Red.into()));
+		frame.render_widget(block, popup_area);
+
+		let inner = Layout::default()
+			.margin(1)
+			.constraints([Constraint::Percentage(100)].as_ref())
+			.split(popup_area)[0];
+
+		let text = Paragraph::new(
+			"Are you sure you want to delete this plan? (y/n)",
+		)
+		.alignment(Alignment::Center)
+		.wrap(Wrap { trim: true });
+		frame.render_widget(text, inner);
+	}
+}
+
+fn centered_rect(percent_x: u16, percent_y: u16, r: Rect) -> Rect {
+	let popup_layout = Layout::default()
+		.direction(Direction::Vertical)
+		.constraints(
+			[
+				Constraint::Percentage((100 - percent_y) / 2),
+				Constraint::Percentage(percent_y),
+				Constraint::Percentage((100 - percent_y) / 2),
+			]
+			.as_ref(),
+		)
+		.split(r);
+	Layout::default()
+		.direction(Direction::Horizontal)
+		.constraints(
+			[
+				Constraint::Percentage((100 - percent_x) / 2),
+				Constraint::Percentage(percent_x),
+				Constraint::Percentage((100 - percent_x) / 2),
+			]
+			.as_ref(),
+		)
+		.split(popup_layout[1])[1]
 }
